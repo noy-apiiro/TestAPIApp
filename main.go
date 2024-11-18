f110d9ed62ef8772221ad6711b32df2bf9dac221
noy wolfson <noy@apiiro.com>
2024-11-18 21:36:13 +0200

    add anonymous test above others

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 func main() {
 	fmt.Println("Welcome!")
 
+	http.HandleFunc("/first", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "This is 3rd anonymous API test!")
+	})
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, this is an anonymous API!")
 	})
